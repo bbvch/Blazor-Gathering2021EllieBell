3935f1c57723d1fec0cbe7f2b61535fa6edc0f90
Othmar Christen <71317597+otb0815@users.noreply.github.com>
2021-10-31 23:33:51 +0100

    Added draft "Grande Mal" Apps

---
+++ sprint1_handsoncode/EllieGglöggli/AlarmService.cs
@@ -0,0 +1,13 @@
+﻿namespace EllieGglöggli
+{
+    public class AlarmService
+    {
+        public string AlarmSent { get; set; } = string.Empty;
+
+        public async Task SendAlarmAsync()
+        {
+            AlarmSent += $"{DateTime.Now.ToShortTimeString()} Alarm sent to: 'local'{Environment.NewLine}";
+            await Task.CompletedTask;
+        }
+    }
+}
