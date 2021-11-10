b580f72d0cfb01553317a94c3a2cf98120b3f6da
Michael Schleiss <michael.schleiss@bbv.ch>
2021-11-10 13:32:48 +0100

    introduced grpc

--- bell/EllieBell/EllieBell/AlarmService.cs
+++ bell/EllieBell/EllieBell/AlarmService.cs
@@ -1,13 +1,70 @@
 ﻿namespace EllieBell
 {
+    using System.Runtime.CompilerServices;
+    using System.Text;
+    using CaseA_GrpsService;
+    using Grpc.Core;
+
     public class AlarmService
     {
+        private const int ClientId = 2;
+
+        private readonly EllieGloeggeli.EllieGloeggeliClient grpcClient;
+
+        public AlarmService(EllieGloeggeli.EllieGloeggeliClient grpcClient)
+        {
+            this.grpcClient = grpcClient;
+        }
             public string AlarmSent { get; set; } = string.Empty;
 
             public async Task SendAlarmAsync()
             {
+
+                await this.grpcClient.PublishGloeggeliAsync(new GloeggeliRequest { SenderClientId = ClientId });
                 AlarmSent += $"{DateTime.Now.ToShortTimeString()} Alarm sent to: 'local'{Environment.NewLine}";
+                
                 await Task.CompletedTask;
             }
+
+            private StringBuilder stringbuider = new StringBuilder();
+            private RegisterRequest ellieRequest;
+
+            private string ServerResponse { get; set; } = string.Empty;
+            public async Task Subscribe()
+            {
+                this.ellieRequest = new RegisterRequest { ClientId = 1, Name = "Ellie" };
+                AsyncServerStreamingCall<GloeggliNotification> streamingCall = this.grpcClient.SubscribeForGloeggli(this.ellieRequest);
+
+                try
+                {
+                    var items = streamingCall.ResponseStream.ReadAllAsync().ConfigureAwait(false);
+                    await foreach (var item in items)
+                    {
+                        this.WriteResponse($"Notification received from {item.SenderClientId}{Environment.NewLine}");
+                    }
+                }
+                catch (RpcException e) when(e.StatusCode == StatusCode.Cancelled)
+                {
+                    WriteResponse("Unsubscription successful");
+                }
+                catch (OperationCanceledException)
+                {
+                    WriteResponse("Unsubscription successful");
+                }
         }
+
+            private void WriteResponse(string response)
+            {
+                lock (this.stringbuider)
+                {
+                    this.stringbuider.AppendLine($"{DateTime.Now.Ticks}: {response}");
+                    ServerResponse = this.stringbuider.ToString();
+                }
+            }
+
+            public async Task Unsubscribe()
+            {
+                await this.grpcClient.UnsubscribeForGloeggliAsync(this.ellieRequest);
+            }
+    }
 }
--- bell/EllieBell/EllieBell/Program.cs
+++ bell/EllieBell/EllieBell/Program.cs
@@ -1,3 +1,4 @@
+using CaseA_GrpsService;
 using EllieBell;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -7,6 +8,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
-                .AddSingleton<AlarmService>();
+                .AddSingleton<AlarmService>()
+                .AddSingleton<EllieGloeggeli.EllieGloeggeliClient>();
 
 await builder.Build().RunAsync();
