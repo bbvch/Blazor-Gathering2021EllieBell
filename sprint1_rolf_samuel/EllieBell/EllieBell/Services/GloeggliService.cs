65a03822dc31081f1b2797c43446738388c1dedd
Samuel Schwager <samuel.schwager@bbv.ch>
2021-11-10 11:06:00 +0100

    Ellies App - Sprint 1

---
+++ sprint1_rolf_samuel/EllieBell/EllieBell/Services/GloeggliService.cs
@@ -0,0 +1,12 @@
+﻿namespace EllieBell.Services
+{
+	public class GloeggliService
+	{
+		public List<string> Messages { get; set; } = new List<string>();
+
+		public void RingGloeggli()
+		{
+			Messages.Add($"{DateTime.Now:o}: Ellie has an epileptic event!");
+		}
+	}
+}
