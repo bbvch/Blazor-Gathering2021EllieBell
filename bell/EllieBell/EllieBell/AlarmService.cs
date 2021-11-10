e6d1821457ea4e64c2c8970d3adf10e6f957896c
Michael Schleiss <michael.schleiss@bbv.ch>
2021-11-10 11:02:37 +0100

    sprint 1

---
+++ bell/EllieBell/EllieBell/AlarmService.cs
@@ -0,0 +1,13 @@
+﻿namespace EllieBell
+{
+    public class AlarmService
+    {
+            public string AlarmSent { get; set; } = string.Empty;
+
+            public async Task SendAlarmAsync()
+            {
+                AlarmSent += $"{DateTime.Now.ToShortTimeString()} Alarm sent to: 'local'{Environment.NewLine}";
+                await Task.CompletedTask;
+            }
+        }
+}
